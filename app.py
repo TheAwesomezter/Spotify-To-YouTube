ab2d18cb9b14979742064669e0d337716d593f88
Viresh Uberoy <48689761+TheAwesomezter@users.noreply.github.com>
2019-10-23 22:40:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import spotipy.util as util
 import csv
 
 user_id = "zspiqhjzpsdh6r8vy5wiftudu" # enter your user id
-client_id = "17019f0ff50848ecbfa9845242da1484"
-client_secret = "fbc525891a8842b8b967aeb7098c6eac"
+client_id = "17019f0ff50848ecbfa9845242da1484" # enter your client ID
+client_secret = "fbc525891a8842b8b967aeb7098c6eac" # enter your client secret
 redirect_url = "http://localhost:8888/"
 
-playlist_id = "5IyU7Wdwb0m7XGlcjDGH5d"
+playlist_id = "5IyU7Wdwb0m7XGlcjDGH5d" # enter the playlist id
 
 try:
     token = util.prompt_for_user_token(user_id, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_url)
@@ -43,8 +43,8 @@ with open('songs_list.csv', 'w+') as file: # enter the file name
     writer = csv.writer(file)
     writer.writerows(songs)
 
-# with open('songs_list.csv') as input, open('modified_list.csv', 'w', newline='') as output:
-#     writer = csv.writer(output)
-#     for row in csv.reader(input):
-#         if any(field.strip() for field in row):
-#             writer.writerow(row)
+with open('songs_list.csv') as input, open('modified_list.csv', 'w', newline='') as output:
+    writer = csv.writer(output)
+     for row in csv.reader(input):
+         if any(field.strip() for field in row):
+             writer.writerow(row)
